1cead6a2b52ba7e6e42a4825a07d89bf9bc5d74e
hsjad7893-cell <hsjad7893@gmail.com>
2026-09-02 12:39:35 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello")
+from telegram.ext import (
+    Application,
+    CommandHandler,
+)
+
+from config import TOKEN
+from database import create_db
+from handlers import start
+
+
+async def post_init(app: Application):
+    await create_db()
+
+
+app = Application.builder().token(TOKEN).post_init(post_init).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("🤖 Bot Started...")
+
+app.run_polling()
