b04cc3f457081af8d6d7e45a4744ae2fd09aacf7
hsjad7893-cell <hsjad7893@gmail.com>
2026-09-02 12:46:07 +0330

    Update admin.py

--- admin.py
+++ admin.py
@@ -29,3 +29,36 @@ async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         f"📊 تعداد کاربران ربات: {count}"
     )
+from database import get_all_users
+
+broadcast_mode = {}
+
+async def broadcast(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id not in ADMINS:
+        return
+
+    broadcast_mode[update.effective_user.id] = True
+    await update.message.reply_text("📢 پیام همگانی را ارسال کن.")
+
+async def handle_broadcast(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id not in broadcast_mode:
+        return
+
+    users = await get_all_users()
+
+    ok = 0
+
+    for user in users:
+        try:
+            await context.bot.copy_message(
+                chat_id=user[0],
+                from_chat_id=update.effective_chat.id,
+                message_id=update.message.message_id
+            )
+            ok += 1
+        except:
+            pass
+
+    del broadcast_mode[update.effective_user.id]
+
+    await update.message.reply_text(f"✅ برای {ok} نفر ارسال شد.")
