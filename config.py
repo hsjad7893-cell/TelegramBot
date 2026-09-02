f367ee2bd2356ce4bb15402a65911b4b6e4af1d6
hsjad7893-cell <hsjad7893@gmail.com>
2026-09-02 15:50:42 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -7,4 +7,15 @@ TOKEN = os.getenv("TOKEN")
 
 CHANNEL = "@GUILD_ALCATRAZ"
 
-ADMINS = [8999545910]
+ADMINS = [
+    8999545910
+]
+
+START_COINS = 50
+
+INVITE_REWARD = 10
+
+HEADSHOT_PRICE = 20
+IPHONE_PRICE = 30
+SAMSUNG_PRICE = 30
+HUD_PRICE = 40
